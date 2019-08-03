acb672c0433cc0ef57d6be75efb4deadc276fa2b
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-08-03 16:00:45 +0700

    solved some more tasks Java Core lvl 4-5

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task14.task1420;
+
+/* 
+НОД
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int first = Integer.parseInt(reader.readLine());
+        if (first <= 0 ) throw new Exception();
+        int second = Integer.parseInt(reader.readLine());
+        if (second <=0) throw new Exception();
+        System.out.println(gcd(first, second));
+    }
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return Math.abs(a);
+        }
+        return gcd(b, a % b);
+    }
+}
